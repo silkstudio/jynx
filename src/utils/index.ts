2f819a1fbfc121bc5456f1f7796378d5f9ec4508
Silk Studio <67189867+silkstudio@users.noreply.github.com>
2021-11-14 19:48:58 +0000

    chore: add sortStyles util

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,7 @@
 // Internal Utils
 export { createMediaQuery } from './createMediaQuery'
 export { merge } from './merge'
+export { sortStyles } from './sortStyles'
 
 // External Utils
 export { default as get } from 'lodash.get'
