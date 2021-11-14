57aaa5df073cc66e72990a787d25116cb2ba45a9
Silk Studio <67189867+silkstudio@users.noreply.github.com>
2021-11-14 16:32:12 +0000

    chore: add ThemeProvider

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,3 @@
-import { baseTheme } from './theme'
-
 export const Greeter = (name: string): string => `Hello ${name}`
 
-export const Meeter = (name: string): string => `Hi ${name}`
-
-export { baseTheme }
+export { ThemeProvider } from './theme'
--- src/theme/ThemeProvider.tsx
+++ src/theme/ThemeProvider.tsx
@@ -1,3 +1,33 @@
-export const ThemeProvider = (): string => {
-  return ''
+import React from 'react'
+
+// Components
+import { ThemeProvider as SCThemeProvider } from 'styled-components'
+
+// Types
+import { BaseExtensibleObject } from '../types'
+
+// Utils
+import { baseTheme } from '.'
+import { merge } from '../utils'
+
+// Props
+interface Props {
+  theme?: BaseExtensibleObject
 }
+
+/*
+
+
+
+
+
+
+
+
+*/
+
+const ThemeProvider: React.FC<Props> = ({ theme, children }) => {
+  return <SCThemeProvider theme={merge(baseTheme, theme)}>{children}</SCThemeProvider>
+}
+
+export { ThemeProvider }
