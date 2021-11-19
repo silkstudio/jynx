e4d82c9ba5cc6f3d29ee8adb840e0d8ce0d8c5da
Silk Studio <67189867+silkstudio@users.noreply.github.com>
2021-11-19 19:46:18 +0000

    chore: add placeholder values to systemTheme

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -2,11 +2,16 @@ export const systemTheme = {
   breakpoints: {
     sm: 640,
     md: 960,
-    lg: 1280
+    lg: 1280,
+    xl: 1366,
+    xxl: 1600
   },
-  spaces: {},
-  fontSizes: {},
-  colors: {},
+  spaces: [0, 4, 8, 16, 32, 64, 128, 256],
+  fontSizes: [12, 14, 16, 24, 32, 48, 64, 96, 128],
+  colors: {
+    blue: '#07c',
+    red: '#e10'
+  },
   fonts: {
     primary: {
       helvetica: '',
@@ -17,14 +22,31 @@ export const systemTheme = {
       hk_grotesk: ''
     }
   },
-  fontWeights: {},
+  fontWeights: {
+    thin: 100,
+    light: 300,
+    regular: 400,
+    medium: 500,
+    semibold: 600,
+    bold: 700,
+    extraBold: 800
+  },
   lineHeights: {},
   letterSpacings: {},
   sizes: {},
   borders: {},
-  borderWidths: {},
-  borderStyles: {},
-  radii: {},
+  borderWidths: {
+    sm: 1,
+    md: 2,
+    lg: 3
+  },
+  borderStyles: ['solid'],
+  radii: [4],
   shadows: {},
-  zIndices: {}
+  zIndices: {
+    main: 1,
+    persistant: 2,
+    modals: 3,
+    notifications: 4
+  }
 }
