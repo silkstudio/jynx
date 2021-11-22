42f76882d5abd0956ad721c8b6a190b719844c29
Silk Studio <67189867+silkstudio@users.noreply.github.com>
2021-11-22 18:04:18 +0000

    refactor: add more default values to theme

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -9,29 +9,32 @@ export const systemTheme = {
   spaces: [0, 4, 8, 16, 32, 64, 128, 256],
   fontSizes: [12, 14, 16, 24, 32, 48, 64, 96, 128],
   colors: {
-    blue: '#07c',
-    red: '#e10'
+    red: '#F2335D',
+    orange: '#FE8400',
+    yellow: '#FFCC00',
+    green: '#7CD420',
+    blue: '#12A5EC',
+    purple: '#7753F8'
   },
   fonts: {
-    primary: {
-      helvetica: '',
-      times: ''
-    },
-    secondary: {
-      inter: '',
-      hk_grotesk: ''
-    }
+    primary: 'helvetica',
+    secondary: 'inter'
   },
   fontWeights: {
     thin: 100,
+    extraLight: 200,
     light: 300,
     regular: 400,
     medium: 500,
     semibold: 600,
     bold: 700,
-    extraBold: 800
+    extraBold: 800,
+    black: 900
   },
-  lineHeights: {},
+  lineHeights: {
+    default: 1.5,
+    spaced: 2
+  },
   letterSpacings: {},
   sizes: {},
   borders: {},
@@ -40,12 +43,12 @@ export const systemTheme = {
     md: 2,
     lg: 3
   },
-  borderStyles: ['solid'],
-  radii: [4],
+  borderStyles: ['solid', 'dashed'],
+  radii: [1, 2, 4, 8],
   shadows: {},
   zIndices: {
     main: 1,
-    persistant: 2,
+    layout: 2,
     modals: 3,
     notifications: 4
   }
