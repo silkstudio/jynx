4ed218bffb5e2459e9d348cc76dac7dd153b979c
Silk Studio <67189867+silkstudio@users.noreply.github.com>
2021-11-28 20:42:06 +0000

    refactor: update test config

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,8 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node'
+  testEnvironment: 'node',
+  collectCoverage: true,
+  coverageReporters: ['text', 'lcov']
 }
---
+++ test/utils/merge.test.ts
@@ -0,0 +1,19 @@
+import { merge } from '../../src/utils/merge'
+
+const foo = {
+  a: 'a',
+  b: 'b',
+  c: 'c'
+}
+
+const bar = {
+  d: 'd',
+  e: 'e',
+  f: 'f'
+}
+
+describe('test merge utility', () => {
+  it('should return combined object of `foo` and `bar`', () => {
+    expect(merge(foo, bar)).toEqual({ a: 'a', b: 'b', c: 'c', d: 'd', e: 'e', f: 'f' })
+  })
+})
