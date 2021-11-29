4c1bb7daf04c72e99de825e300421c29d6dadae5
Silk Studio <67189867+silkstudio@users.noreply.github.com>
2021-11-29 10:16:35 +0000

    chore: add tests for isNumber util

---
+++ test/utils/isNumber.test.ts
@@ -0,0 +1,19 @@
+import { isNumber } from '../../src/utils'
+
+describe('test isNumber utility', () => {
+  test('should return true if the value passed to testit is a number', () => {
+    expect(isNumber(2)).toBe(true)
+  })
+
+  it('should return true if the value passed to it is a string of a number', () => {
+    expect(isNumber('2')).toBe(true)
+  })
+
+  it('should return false if the value passed to it is not a number', () => {
+    expect(isNumber('not a number')).toBe(false)
+  })
+
+  it('should return false if the value passed to it is undefined', () => {
+    expect(isNumber(undefined)).toBe(false)
+  })
+})
