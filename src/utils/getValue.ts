34d0d867703bba422104884dea7d8de311941350
Silk Studio <67189867+silkstudio@users.noreply.github.com>
2021-11-30 19:25:36 +0000

    chore: create getValue util

---
+++ src/utils/getValue.ts
@@ -0,0 +1,32 @@
+// Types
+import { DefaultTheme } from '../types'
+
+// Utils
+import { get } from '.'
+
+/**
+ *
+ * @param {string | number} value
+ * @param {DefaultTheme[keyof DefaultTheme]} scale
+ * @returns {string | number | Record<string, any>}
+ */
+
+/*
+
+
+
+
+
+
+
+
+*/
+
+const getValue = (value?: string | number, scale?: DefaultTheme[keyof DefaultTheme]): string | number | Record<string, any> => {
+  if (scale) {
+    return value ? get(scale, value, value) : ''
+  }
+  return value || ''
+}
+
+export { getValue }
