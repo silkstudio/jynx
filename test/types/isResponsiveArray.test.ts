596b63eb0611228c2e23bf0649739ec31135c9f8
Silk Studio <67189867+silkstudio@users.noreply.github.com>
2021-12-01 10:39:56 +0000

    chore: create tests for responsive array type guard

---
+++ test/types/isResponsiveArray.test.ts
@@ -0,0 +1,46 @@
+import { isResponsiveArray } from '../../src/types'
+
+const check = ['blue', 'red', 'green']
+
+const breakpoints = [640, 960, 1280, 1366]
+
+/*
+
+
+
+
+
+
+
+
+*/
+
+describe('isResponsiveArray type guard', () => {
+  it('should return a boolean', () => {
+    expect(typeof isResponsiveArray<string>(check)).toBe('boolean')
+  })
+
+  it('should return a true if an array of the specified type is passed', () => {
+    expect(isResponsiveArray<string>(check)).toBe(true)
+  })
+
+  it('should return a false if an array is passed it has no values', () => {
+    expect(isResponsiveArray<string>([])).toBe(false)
+  })
+
+  it('should return a false if the value passed is not an array', () => {
+    expect(isResponsiveArray<string>('foo')).toBe(false)
+    expect(isResponsiveArray<string>(300)).toBe(false)
+    expect(isResponsiveArray<string>(() => 'bar')).toBe(false)
+    expect(isResponsiveArray<string>({ blue: 'blue', red: 'red', green: 'green' })).toBe(false)
+    expect(isResponsiveArray<string>(null)).toBe(false)
+    expect(isResponsiveArray<string>(undefined)).toBe(false)
+  })
+
+  it('should still return true, even if there are more values in the array that there are breakpoints in the theme', () => {
+    const foo = [...check, ...check, ...check]
+
+    expect(breakpoints.length < foo.length)
+    expect(isResponsiveArray<string>(foo)).toBe(true)
+  })
+})
