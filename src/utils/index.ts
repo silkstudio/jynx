b95b5459be56ffd7b9cd4e342cf9e25e4460c048
Silk Studio <67189867+silkstudio@users.noreply.github.com>
2021-12-13 16:36:57 +0000

    chore: create breakpoint parser

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,7 @@
 // Internal Utils
 export { addUnitIfNeeded } from './addUnitIfNeeded'
 export { createMediaQuery } from './createMediaQuery'
+export { defaultBreakpoints } from './defaultBreakpoints'
 export { getValue } from './getValue'
 export { isNumber } from './isNumber'
 export { merge } from './merge'
