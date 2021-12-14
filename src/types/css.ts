cce34bed4024ed4f07fcddf4c9cc8b068f9a4bc7
Silk Studio <67189867+silkstudio@users.noreply.github.com>
2021-12-14 09:22:59 +0000

    chore: create css type file

---
+++ src/types/css.ts
@@ -0,0 +1,62 @@
+import * as CSS from 'csstype'
+
+/*
+
+
+
+
+
+
+
+
+*/
+
+/**
+ * CSSProperties
+ *
+ * @since 1.0.0
+ */
+interface CSSProperties extends CSS.StandardProperties<number | string>, CSS.SvgProperties<number | string> {}
+
+/**
+ * CSSPseudos
+ *
+ * @since 1.0.0
+ */
+export type CSSPseudos = { [K in CSS.Pseudos]?: CSSObject }
+
+/**
+ * CSSPropertiesWithMultiValues
+ *
+ * @since 1.0.0
+ */
+type CSSPropertiesWithMultiValues = {
+  [K in keyof CSSProperties]: CSSProperties[K]
+}
+
+/**
+ * CSSInterpolation
+ *
+ * @since 1.0.0
+ */
+type CSSInterpolation = undefined | number | string | CSSObject
+
+/**
+ * CSSOthersObjectForCSSObject
+ *
+ * @since 1.0.0
+ */
+interface CSSOthersObjectForCSSObject {
+  [propertiesName: string]: CSSInterpolation
+}
+
+/**
+ * CSSObject
+ *
+ * CSS as POJO that is compatible with CSS-in-JS libaries.
+ * Copied directly from [emotion](https://github.com/emotion-js/emotion/blob/ca3ad1c1dcabf78a95b55cc2dc94cad1998a3196/packages/serialize/types/index.d.ts#L45) types
+ *
+ * @since 1.0.0
+ */
+
+export interface CSSObject extends CSSPropertiesWithMultiValues, CSSPseudos, CSSOthersObjectForCSSObject {}
