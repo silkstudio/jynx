365e66d083419ee591a83bffc83744c022deacc7
Silk Studio <67189867+silkstudio@users.noreply.github.com>
2021-12-15 18:00:22 +0000

    refactor: move flexbox aliases to `extended` function

---
+++ src/functions/extendedFlexbox.ts
@@ -0,0 +1,51 @@
+// Types
+import type { StyledFunction, StyledFunctionConfig } from '../types/functions'
+
+// Utils
+import { flexboxConfig, FlexboxProps } from './flexbox'
+import { createStyles } from '../constructors'
+
+/*
+
+
+
+
+
+
+
+
+*/
+
+const config: StyledFunctionConfig = flexboxConfig
+
+interface ExtendedFlexboxProps extends FlexboxProps {
+  direction?: FlexboxProps['flexDirection']
+  flow?: FlexboxProps['flexFlow']
+  wrap?: FlexboxProps['flexWrap']
+  align?: FlexboxProps['alignItems']
+  justify?: FlexboxProps['justifyContent']
+  grow?: FlexboxProps['flexGrow']
+  shrink?: FlexboxProps['flexShrink']
+  basis?: FlexboxProps['flexBasis']
+}
+
+const extendedFlexbox: StyledFunction<ExtendedFlexboxProps> = ({ theme, ...styles }) => {
+  const result = createStyles<ExtendedFlexboxProps>(styles, theme, config)
+
+  if (Object.keys(styles).length) {
+    result.display = 'flex'
+  }
+
+  return result
+}
+
+config.direction = config.flexDirection
+config.flow = config.flexFlow
+config.wrap = config.flexWrap
+config.align = config.alignItems
+config.justify = config.justifyContent
+config.grow = config.flexGrow
+config.shrink = config.flexShrink
+config.basis = config.flexBasis
+
+export { extendedFlexbox, ExtendedFlexboxProps }
--- src/functions/flexbox.ts
+++ src/functions/flexbox.ts
@@ -59,24 +59,16 @@ const config: StyledFunctionConfig = {
 
 interface FlexboxProps {
   flexDirection?: CSSProperties['flexDirection'] | ResponsiveStyle<CSSProperties['flexDirection']>
-  direction?: CSSProperties['flexDirection'] | ResponsiveStyle<CSSProperties['flexDirection']>
   flexFlow?: CSSProperties['flexFlow'] | ResponsiveStyle<CSSProperties['flexFlow']>
-  flow?: CSSProperties['flexFlow'] | ResponsiveStyle<CSSProperties['flexFlow']>
   flexWrap?: CSSProperties['flexWrap'] | ResponsiveStyle<CSSProperties['flexWrap']>
-  wrap?: CSSProperties['flexWrap'] | ResponsiveStyle<CSSProperties['flexWrap']>
   alignItems?: CSSProperties['alignItems'] | ResponsiveStyle<CSSProperties['alignItems']>
-  align?: CSSProperties['alignItems'] | ResponsiveStyle<CSSProperties['alignItems']>
   alignContent?: CSSProperties['alignContent'] | ResponsiveStyle<CSSProperties['alignContent']>
   alignSelf?: CSSProperties['alignSelf'] | ResponsiveStyle<CSSProperties['alignSelf']>
   justifyContent?: CSSProperties['justifyContent'] | ResponsiveStyle<CSSProperties['justifyContent']>
-  justify?: CSSProperties['justifyContent'] | ResponsiveStyle<CSSProperties['justifyContent']>
   gap?: CSSProperties['gap'] | ResponsiveStyle<CSSProperties['gap']>
   flexGrow?: CSSProperties['flexGrow'] | ResponsiveStyle<CSSProperties['flexGrow']>
-  grow?: CSSProperties['flexGrow'] | ResponsiveStyle<CSSProperties['flexGrow']>
   flexShrink?: CSSProperties['flexShrink'] | ResponsiveStyle<CSSProperties['flexShrink']>
-  shrink?: CSSProperties['flexShrink'] | ResponsiveStyle<CSSProperties['flexShrink']>
   flexBasis?: CSSProperties['flexBasis'] | ResponsiveStyle<CSSProperties['flexBasis']>
-  basis?: CSSProperties['flexBasis'] | ResponsiveStyle<CSSProperties['flexBasis']>
   order?: CSSProperties['order'] | ResponsiveStyle<CSSProperties['order']>
 }
 
@@ -90,13 +82,4 @@ const flexbox: StyledFunction<FlexboxProps> = ({ theme, ...styles }) => {
   return result
 }
 
-config.direction = config.flexDirection
-config.flow = config.flexFlow
-config.wrap = config.flexWrap
-config.align = config.alignItems
-config.justify = config.justifyContent
-config.grow = config.flexGrow
-config.shrink = config.flexShrink
-config.basis = config.flexBasis
-
-export { flexbox, FlexboxProps }
+export { flexbox, config as flexboxConfig, FlexboxProps }
