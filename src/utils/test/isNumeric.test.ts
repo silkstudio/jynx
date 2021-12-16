dbd18653c07620d8e4ef08b3abdbfb0e14c93cd7
Silk Studio <67189867+silkstudio@users.noreply.github.com>
2021-12-16 12:13:18 +0000

    refactor: fix name of `isNumeric` util export

--- src/utils/test/isNumeric.test.ts
+++ src/utils/test/isNumeric.test.ts
@@ -1,4 +1,4 @@
-import { isNumber } from '../index'
+import { isNumeric } from '../isNumeric'
 /*
 
 
@@ -12,33 +12,33 @@ import { isNumber } from '../index'
 
 describe('When the value passed is numeric', () => {
   it('should return true', () => {
-    expect(isNumber(2)).toBe(true)
+    expect(isNumeric(2)).toBe(true)
   })
 })
 
 describe('When the value passed is a numeric string', () => {
   it('should return true', () => {
-    expect(isNumber('2')).toBe(true)
+    expect(isNumeric('2')).toBe(true)
   })
 })
 
 describe('When the value passed is a non-numeric string', () => {
   it('should return false', () => {
-    expect(isNumber('not a number')).toBe(false)
+    expect(isNumeric('not a number')).toBe(false)
   })
 })
 
 describe('When the value passed is not a number or a string', () => {
   it('should return false when passed null', () => {
-    expect(isNumber(null)).toBe(false)
+    expect(isNumeric(null)).toBe(false)
   })
 
   it('should return false when passed undefined', () => {
-    expect(isNumber(undefined)).toBe(false)
+    expect(isNumeric(undefined)).toBe(false)
   })
 
   it('should return false when passed a boolean', () => {
-    expect(isNumber(true)).toBe(false)
-    expect(isNumber(false)).toBe(false)
+    expect(isNumeric(true)).toBe(false)
+    expect(isNumeric(false)).toBe(false)
   })
 })
