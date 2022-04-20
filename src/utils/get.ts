317931775daed906909e6573e4dd1b86a2bdd42d
nathnhughes <nathan@silkstudio.co>
2022-04-20 17:28:02 +0100

    fix: fix get util

--- src/utils/get.ts
+++ src/utils/get.ts
@@ -1,15 +1,4 @@
-/**
- * Utility function tat retrieves a nested property from an object based on string or numeric path
- *
- * @template T [=any] Used to define the return type of the function
- *
- * @param {any} obj Object which the required value is nested within
- * @param {string | number} path String or numeric path to the desired property
- * @param {T} fallback A value to use if the provided path does not lead to a value
- * @returns {T} Either the value found at the specified path or the fallback value
- *
- * @since 1.1.0
- */
+import _get from 'lodash.get'
 
 /*
 
@@ -22,20 +11,6 @@
 
 */
 
-const get = <T = any>(obj: any, path: string | number, fallback?: T): T => {
-  const key = typeof path === 'string' ? path.split('.') : [path]
-
-  let result: any = obj
-
-  for (let i = 0; i < key.length; i += 1) {
-    if (!result) {
-      break
-    }
-
-    result = result[key[i]]
-  }
-
-  return result === undefined ? fallback : result
-}
+const get = _get
 
 export { get }
